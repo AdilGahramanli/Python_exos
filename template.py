4e81ff336cb611341246d05bf72012d8980d0663
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2024-02-08 16:53:45 +0100

    tri des villes

--- template.py
+++ template.py
@@ -1,18 +1,35 @@
 def swap(A, i, j):
    A[i], A[j] = A[j], A[i]
 
-# def insertionSort(inputArr):
-#     print("implement me")
 
-# def selectionSort(inputArr):
-#     print("implement me")
+
+def insertionSort(inputArr):
+    for i in range(1, len(inputArr)): # boucle qui avance
+        j=i
+        while j>0 and inputArr[j-1]>inputArr[j]:
+            swap(inputArr, j, j-1)
+            j=j-1 # fait reculer le j
+        
+    return inputArr
+
+def selectionSort(inputArr):
+    nb = len(inputArr)
+    for temp in range(0, nb):                   #temp sélectionnera chaque valeur du tableau
+        mini=temp                               #initialement, temp est considérée comme mini
+        for j in range(temp+1,nb):              #j va chercher toutes les valeurs à droite de temp
+            if inputArr[j] < inputArr[mini]:    #j compare sa valeur avec mini
+                mini = j                        #si j trouve une valeur moindre alors mini prend la même valeur que j
+        if mini != temp:                        #fin de la boucle, on a trouvé la valeur minimale
+            inputArr[temp] = inputArr[mini]     #et on va la placer à l'endroit du temp
+            inputArr[mini] = temp               #on replace mini sur temps pour pouvoir chercher le nouveau mini
+    return inputArr
 
 def bubbleSort(list):
     permutation = True
     while   permutation==True:
         permutation = False
         for i in range(0, len(list)-1):
-            if i<9 and list[i+1] < list[i]:
+            if list[i+1] < list[i]:
                 swap(list,i,i+1)
                 permutation = True
     return list
@@ -33,13 +50,14 @@ list = [random.randint(0, list_size * 2) for _ in range(list_size)]
 swapped_list = list.copy()
 swap(swapped_list, 0, 1)
 
-# insertion_list = insertionSort(list.copy())
 
-# selection_list = selectionSort(list.copy())
+insertion_list = insertionSort(list.copy())
 
-bubble_list = bubbleSort(swapped_list)
+selection_list = selectionSort(list.copy())
 
-# shell_list = shellSort(list.copy())
+bubble_list = bubbleSort(list.copy())
+
+shell_list = shellSort(list.copy())
 
 # heap_list = heapSort(list.copy())
 
@@ -50,17 +68,17 @@ bubble_list = bubbleSort(swapped_list)
 
 # print(swapped_list)
 
-# print("Insertion")
-# print(insertion_list)
+print("Insertion")
+print(insertion_list)
 
-# print("Selection")
-# print(selection_list)
+print("Selection")
+print(selection_list)
 
 print("Bubble")
 print(bubble_list)
 
-# print("Shell")
-# print(shell_list)
+print("Shell")
+print(shell_list)
 
-# print("Heap")
+print("Heap")
 # print(heap_list)
